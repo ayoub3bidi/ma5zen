602ef365ecbecb62c90cbbc0c3c0b61b73a910a1
ayoub3bidi <mrayoubabidi@gmail.com>
2022-12-27 14:54:32 +0100

    💄 style: add navbar links

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -13,9 +13,16 @@ import {
     Avatar,
     useMantineTheme
 } from '@mantine/core';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
 import React from 'react'
 import { GoArchive } from 'react-icons/go';
 import { ImIcoMoon, ImSun } from 'react-icons/im';
+import { AiOutlineHome } from 'react-icons/ai';
+import { BiCategory } from 'react-icons/bi';
+import { BsBox } from 'react-icons/bs';
+import { FiSettings } from 'react-icons/fi';
+import { TbClipboardList } from 'react-icons/tb';
 
 const Brand = () => {
     const {colorScheme, toggleColorScheme} = useMantineColorScheme();
@@ -38,7 +45,7 @@ const Brand = () => {
                     >
                         <GoArchive size={18}/>
                     </ThemeIcon>
-                    <Title size={'1.2rem'} weight={400}>Inventory</Title>
+                    <Title size={'1.2rem'} weight={400}>Ma5zen</Title>
                 </Group>
                 {/* TODO: add light/dark mode */}
                 <ActionIcon variant='default' onClick={() =>  toggleColorScheme()} size={30}>
@@ -48,6 +55,77 @@ const Brand = () => {
         </Box>
     )
 }
+
+interface MainLinksProps {
+    icon: React.ReactNode;
+    label: string;
+    color: string;
+    pageLink: string;
+}
+
+const MainLinks = ({icon, label, color, pageLink}: MainLinksProps) => {
+    const {pathname} = useRouter();
+    return (<Link href={pageLink} passHref>
+        <UnstyledButton sx={(theme) => ({
+            display: 'block',
+            width: '100%',
+            padding: theme.spacing.xs,
+            borderRadius: theme.radius.sm,
+            color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.colors.black,
+            backgroundColor:
+            pathname === pageLink
+                ? theme.colorScheme === 'dark'
+                    ? theme.colors.dark[6]
+                    : theme.colors.gray[0]
+                : 'transparent',
+            "& hover": {
+                backgroundColor:
+                    theme.colorScheme === 'dark'
+                        ? theme.colors.dark[6]
+                        : theme.colors.gray[0],
+            }
+        })}>
+            <Group>
+                <ThemeIcon color={color} variant={'light'}>{icon}</ThemeIcon>
+                <Text size='sm'>{label}</Text>
+            </Group>
+        </UnstyledButton>
+    </Link>
+    );
+};
+
+const data: MainLinksProps[] = [
+    {
+        icon: <AiOutlineHome size={18}/>,
+        color: 'blue',
+        label: 'Home',
+        pageLink: '/',
+    },
+    {
+        icon: <BiCategory size={18}/>,
+        color: 'teal',
+        label: 'Categories',
+        pageLink: '/categories',
+    },
+    {
+        icon: <BsBox size={16}/>,
+        color: 'violet',
+        label: 'Inventory',
+        pageLink: '/inventory',
+    },
+    {
+        icon: <TbClipboardList size={20}/>,
+        color: 'grape',
+        label: 'Products',
+        pageLink: '/products',
+    },
+    {
+        icon: <FiSettings size={16}/>,
+        color: 'orange',
+        label: 'Settings',
+        pageLink: '/settings',
+    },
+]
 
 const Nav = ({opened, hiddenBreakpoint} : {opened: boolean; hiddenBreakpoint: MantineNumberSize}) => {
   return (
@@ -61,7 +139,9 @@ const Nav = ({opened, hiddenBreakpoint} : {opened: boolean; hiddenBreakpoint: Ma
             <Brand/>
         </Navbar.Section>
         <Navbar.Section grow mt='md'>
-            Links
+            {data.map((item, index) => (
+                <MainLinks key={index} {...item}/>
+            ))}
         </Navbar.Section>
         <Navbar.Section>
             Avatar
