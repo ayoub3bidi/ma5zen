2c0f2694578734c83179facc7e79e180b7922f6e
ayoub3bidi <mrayoubabidi@gmail.com>
2022-12-27 14:11:29 +0100

    💄 style: add brand component

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -14,6 +14,40 @@ import {
     useMantineTheme
 } from '@mantine/core';
 import React from 'react'
+import { GoArchive } from 'react-icons/go';
+import { ImIcoMoon, ImSun } from 'react-icons/im';
+
+const Brand = () => {
+    const {colorScheme, toggleColorScheme} = useMantineColorScheme();
+    return (
+        <Box
+            sx={(theme) => ({
+                paddingLeft: theme.spacing.xs,
+                paddingRight: theme.spacing.xs,
+                paddingBottom: theme.spacing.lg,
+                borderBottom: `1px solid ${theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[2]}`,
+            })}
+        >
+            <Group position='apart' align='center'>
+                <Group>
+                    <ThemeIcon
+                        variant="gradient"
+                        gradient={{from: 'indigo', to: 'cyan'}}
+                        size="lg"
+                        radius="lg"
+                    >
+                        <GoArchive size={18}/>
+                    </ThemeIcon>
+                    <Title size={'1.2rem'} weight={400}>Inventory</Title>
+                </Group>
+                {/* TODO: add light/dark mode */}
+                <ActionIcon variant='default' onClick={() =>  toggleColorScheme()} size={30}>
+                    { colorScheme === 'dark' ? <ImSun size={18}/> : <ImIcoMoon size={18}/> }
+                </ActionIcon>
+            </Group>
+        </Box>
+    )
+}
 
 const Nav = ({opened, hiddenBreakpoint} : {opened: boolean; hiddenBreakpoint: MantineNumberSize}) => {
   return (
@@ -24,7 +58,7 @@ const Nav = ({opened, hiddenBreakpoint} : {opened: boolean; hiddenBreakpoint: Ma
         hidden={!opened}
     >
         <Navbar.Section mt='xs'>
-            Ma5zen
+            <Brand/>
         </Navbar.Section>
         <Navbar.Section grow mt='md'>
             Links
