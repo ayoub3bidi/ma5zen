ba7d0a20a0b7a368c296561a05920db218af2c47
ayoub3bidi <mrayoubabidi@gmail.com>
2022-12-31 11:16:57 +0100

    ✅ feat: add search component

--- pages/categories.tsx
+++ pages/categories.tsx
@@ -20,9 +20,24 @@ import { BiCategory } from 'react-icons/bi'
 import { FiSearch } from 'react-icons/fi'
 import { MdWarningAmber } from 'react-icons/md'
 import { useGetCategories } from '../queries/CategoryQueries'
+import { GetCategory } from '../types/getCategories'
 
 const Categories:CustomNextPage = () => {
   const { data: categories, isLoading: categoriesLoading } = useGetCategories();
+  // SEARCH VALUE & DATA
+  const [selectData, setSelectData] = useState<GetCategory['name'][]>([]);
+  const [selectValue, setSelectValue] = useState<GetCategory['name'] | null>(null);
+  // FILTERED VALUES
+  const [filteredValue, setFilteredValue] = useState<GetCategory[]>();
+  // SET SELECT DATA FOR SEARCH
+  useEffect(() => {
+    setSelectData([]);
+    if(categories) {
+      categories.map(category=>setSelectData((selectData)=>[...selectData, category.name]));
+    }
+    setFilteredValue(categories);
+  }, [categories]);
+
   return (
     <main>
       <Group align='center' mb='3rem'>
@@ -33,7 +48,10 @@ const Categories:CustomNextPage = () => {
           Your Categories
         </Title>
       </Group>
-      {/* <Select
+      <Select
+        data={selectData}
+        value={selectValue}
+        onChange={setSelectValue}
         clearable
         searchable
         nothingFound="No Categories Found"
@@ -44,7 +62,18 @@ const Categories:CustomNextPage = () => {
         sx={{maxWidth: '600px'}}
         mb='1rem'
         placeholder='Search Categories'
-      /> */}
+      />
+      {/* NO CATEGORIES FOUND */}
+      {
+        categories?.length === 0
+        && !categoriesLoading
+        && (<Box>
+          <Group align='center'>
+            <Text>No Inventory/Categories</Text>
+            <FiSearch size={20} />
+          </Group>
+        </Box>)
+      }
     </main>
   )
 }
